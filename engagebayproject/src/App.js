2d29b37f7198b3aea22a09863c3515bd2ac64f75
Ayushi <ayushivashisth22@gmail.com>
2022-12-14 22:09:06 +0530

    day-1

---
+++ engagebayproject/src/App.js
@@ -0,0 +1,34 @@
+//import logo from './logo.svg';
+import './App.css';
+// import Navbar from './Components/Navbar';
+// import VideoBackGround from './Components/HomeComponents/VideoBackGround';
+// import AvtivitySoution from './Components/HomeComponents/AvtivitySolution';
+// import ReviewsTab from './Components/HomeComponents/ReviewsTab';
+// import CreateOwnViews from './Components/HomeComponents/CreateOwnViews';
+// import ClinetTestimonial from './Components/HomeComponents/ClinetTestimonial';
+// import BoxHover from './Components/HomeComponents/BoxHover';
+// import GetStartedBox from './Components/HomeComponents/GetStartedBox';
+// import Footer from './Components/Footer';
+import AllRoutes from './Components/AllRoutes';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes/>
+       {/* <Navbar/>
+       <VideoBackGround/>
+       <AvtivitySoution/>
+       <ReviewsTab/>
+       <CreateOwnViews/>
+       <ClinetTestimonial/>
+       <BoxHover/>
+       <GetStartedBox/>
+       <Footer/> */}
+       
+    </div>
+  );
+}
+
+export default App;
+
